e5ff0545f573d232c0a4287c62a8ef3adaf86df5
Matthias Vegh <matyas.vegh@gmail.com>
2019-04-14 00:03:51 +0200

    Add schema to appendix and validate against it

--- test/robot/dump-names/libraries/Variables.py
+++ test/robot/dump-names/libraries/Variables.py
@@ -1,9 +1,14 @@
 import json
+import jsonschema
+import os
 
 class Variables(object):
     def __init__(self):
-        pass
+        self.schema = json.load(open(
+                os.path.join(os.path.dirname(__file__),
+                        '..', '..', '..', '..', 'schema', 'varnames.json')))
 
     def load_variables_from_analysis(self, content):
         d = json.loads(content)
+        jsonschema.validate(instance=d, schema=self.schema)
         return d['Variables']
